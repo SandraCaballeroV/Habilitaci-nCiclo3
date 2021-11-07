9b0c144d029f2c68e4064e95a6d94c511b18a8da
unknown <sandracaballero11@gmail.com>
2021-11-07 01:10:58 -0500

    correccion de imagen

--- src/components/ImagenLogo.jsx
+++ src/components/ImagenLogo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from 'media/logo.png';
+import Logo from '../media/logo.png';
 
 const ImagenLogo = () => {
   return <img className='mx-auto h-40 w-auto' src={Logo} alt='Workflow' />;

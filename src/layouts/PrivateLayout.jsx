e1c607710858c30e08fad33aa689d1397eb55eff
unknown <sandracaballero11@gmail.com>
2021-11-10 21:44:54 -0500

    correccion SiderResponsive

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Sidebar from 'components/Sidebar';
-import SidebarResponsive from 'components/SidebarResponsive';
+import SidebarResponsive from 'components/SiderbarResponsive';
 import { useAuth0 } from '@auth0/auth0-react';
 import ReactLoading from 'react-loading';
 import { obtenerDatosUsuario } from 'utils/api';
 import { useUser } from 'context/userContext';
+
+
 const PrivateLayout = ({ children }) => {
   const { isAuthenticated, isLoading, loginWithRedirect, getAccessTokenSilently, logout } =
     useAuth0();

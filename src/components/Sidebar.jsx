0e632a19f287fa87a07fc49a0a20c2ea8a9c446c
unknown <sandracaballero11@gmail.com>
2021-11-11 03:32:04 -0500

    arreglo nueva url

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
   const { user, logout } = useAuth0();
 
   const cerrarSesion = () => {
-    logout({ returnTo: 'http://localhost:3000/admin' });
+    logout({ returnTo: 'https://whispering-bastion-16692.herokuapp.com/admin' });
     localStorage.setItem('token', null);
   };
 
